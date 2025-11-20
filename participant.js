ad8c02862c5c6cc7a371276fcc71fc2e595e894f
mbarulli <marco@capsule.social>
2025-11-20 17:20:49 +0100

    Fix incorrect Christmas background image

--- participant.js
+++ participant.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const christmasImages = [
         'https://images.unsplash.com/photo-1513111534-0241805b9280?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
         'https://images.unsplash.com/photo-1512626124831-a497799777e3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-        'https://images.unsplash.com/photo-1511920170033-f8396924c348?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+        'https://images.unsplash.com/photo-1542992199-805b8a32428c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     ];
 
     const randomImage = christmasImages[Math.floor(Math.random() * christmasImages.length)];
